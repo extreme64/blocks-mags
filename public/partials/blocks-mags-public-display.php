5f1be0615e7b4226adc17dcfc9bbadd07f352bfe
Mastermind <adam.gicevic@gmail.com>
2023-04-08 20:47:05 +0200

    Repalced a btn variable with a new name.

--- public/partials/blocks-mags-public-display.php
+++ public/partials/blocks-mags-public-display.php
@@ -16,18 +16,18 @@ if (!defined('ABSPATH')) exit;
 <div class="casino-score-block">
     <div class="casino-score-block__inner border-radius-25">
         <div class="casino-score-block__scores">
-            <b class="casino-score-block__title"> <?php _e('CASINO SCORES', self::$int_domain) ?></b>
+            <b class="casino-score-block__title"> <?php _e('CASINO SCORES', $this->domain) ?></b>
             <div class="casino-score-block__items">
                 <?php echo $html_str ?>
             </div>
         </div>
         <div class="casino-score-block__overall">
-            <b class="casino-score-block__title"><?php _e('OVERALL', self::$int_domain) ?></b><div class="casino-score-block__value-wrap">
+            <b class="casino-score-block__title"><?php _e('OVERALL', $this->domain) ?></b><div class="casino-score-block__value-wrap">
                 <div class="casino-score-block__value">
                     <p><?php echo $overall_rating ?></p>
                 </div>
             </div>
         </div>
     </div>
-    <?php echo $btn ?>
+    <?php echo $template_cta_btn ?>
 </div>

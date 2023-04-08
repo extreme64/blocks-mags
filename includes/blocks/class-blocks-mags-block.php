f2c18d551c16867839b4a3c44e2874f212ce9fd0
Mastermind <adam.gicevic@gmail.com>
2023-04-08 16:32:03 +0200

    No init(). Leave it to children to def. it.

--- includes/blocks/class-blocks-mags-block.php
+++ includes/blocks/class-blocks-mags-block.php
@@ -5,7 +5,7 @@ if (!defined('ABSPATH')) exit;
  * A parent class for all the custom blocks
  * @since 1.1.4
  */
-class Blocks_Mag_Block
+ class Blocks_Mag_Block
 {
     /**
      * The name of the block.
@@ -41,19 +41,8 @@ class Blocks_Mag_Block
         $this->name = $name;
         $this->enqueue_tag = $enqueue_tag;
     }
-    // /**
-    //  *Initializes the block in the WordPress editor.
-    //  * 
-    //  * @since 1.1.4
-    //  */
-    // public function init()
-    // {
-    //     register_block_type($this->name , array(
-    //         'editor_script' => $this->enqueue_tag,
-    //         'editor_style' => $this->enqueue_tag,
-    //         'render_callback' => array($this, 'render'),
-    //     ));
-    // }
+
+
 }
 
 
